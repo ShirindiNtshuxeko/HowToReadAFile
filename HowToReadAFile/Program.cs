4ab324784c252f8b46a965ac9a7e1f7c083c38c3
User <User@LDIL-17>
2024-11-06 09:52:08 +0200

    Add project files.

---
+++ HowToReadAFile/Program.cs
@@ -0,0 +1,27 @@
+﻿namespace HowToReadAFile
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string DocPath = Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments);
+            string TextPath = Path.Combine(DocPath, "New.txt");
+
+            //Check if file exist
+            if (File.Exists(TextPath))
+            {
+                //Read and display the data from the file
+                using (StreamReader sr = new StreamReader(TextPath))
+                {
+                    string fileContent = sr.ReadToEnd();
+                    Console.WriteLine("Contents of text file new");
+                    Console.WriteLine(fileContent);
+                }
+            }
+            else 
+            {
+                Console.WriteLine("File does not exist");
+            }
+        }
+    }
+}
